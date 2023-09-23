95a66f4c2ad815605f1479bf97ce95f20040771c
cyberkitaec <addkudi@mail.ru>
2023-09-23 03:38:10 +0300

    New AccountManager(create user and super user) + admin models

--- software_center/notes/admin.py
+++ software_center/notes/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    """
+    Админка пользователя. В админке не будет отоброжаться password юзера
+    """
+    list_display = ['id', 'name', 'surname', 'date_of_registration', 'date_of_birthday']
+    list_display_links = ['id', 'name', 'surname', 'date_of_registration', 'date_of_birthday']
+    search_fields = ['id', 'name', 'surname', 'date_of_registration', 'date_of_birthday']
+
+
+class EventAdmin(admin.ModelAdmin):
+    """
+    Админка событий.
+    """
+    list_display = '__all__'
+    list_display_links = '__all__'
+    search_fields = '__all__'
+
+
+# Регистрируем админки
+
+admin.site.register(User)
+admin.site.register(Event)
--- software_center/notes/models.py
+++ software_center/notes/models.py
@@ -1,13 +1,28 @@
-from django.contrib.auth.models import AbstractBaseUser, UserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 from django.contrib.auth.hashers import make_password
 from django.core.validators import RegexValidator
 
 # Create your models here.
 
+# Кастомный AccountManager, с помощью него можно будет создавать юзера. В том числе и superuser
+class AccountManager(BaseUserManager):
+    def create_user(self, login, password=None, **extra_fields):
+        if not login:
+            raise ValueError('такой пользователь уже существует')
 
-# Сущность "Пользователь". Создаем свою base-model на основе AbstractBaseUser для корректной работы JWT
-class User(AbstractBaseUser):
+        user = self.model(login=login, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    # Переопределим создание superuser`a. Т.к. если мы будем создавать его через create_user
+    # то получится что мы хэшируем пароль superuser`a два раза, т.к. по дефолту он хэшируется автоматически.
+    def create_superuser(self, login, email=None, password=None, **extra_fields):
+        return User.objects.create(login=login, password=password, is_staff = True, is_admin = True, is_superuser=True)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
 
     login = models.CharField(u'Логин',max_length=30, unique=True)
     password = models.CharField(max_length=128,
@@ -21,25 +36,35 @@ class User(AbstractBaseUser):
                                 )
     name = models.CharField(u'Имя', max_length=30)
     surname = models.CharField(u'Фамилия', max_length=30)
-    date_of_registration = models.DateField(u'Дата регистрации')
+    date_of_registration = models.DateField(u'Дата регистрации', auto_created=True, blank=True, auto_now_add=True)
     date_of_birthday = models.DateField(u'Дата рождения', blank=True, null=True, default=None)
     is_staff = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_superuser = models.BooleanField(default=False)
     last_login = None
 
     USERNAME_FIELD = 'login'
-    objects = UserManager()
+    objects = AccountManager()
 
     class Meta:
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователь'
-
+    #
     # Хешируем пароль. По умолчанию это SHA256
     def save(self, *args, **kwargs):
         self.password = make_password(self.password)
         super(User, self).save(*args, **kwargs)
 
-    def natural_key(self):
-        return self.name
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+    # @property
+    # def is_staff(self):
+    #     return self.is_admin
+
 
 
 # Сущность "Событие"
